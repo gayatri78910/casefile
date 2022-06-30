29ac24809c11e808c5ac0469183ae596cfcd735a
gayatri78910 <104496710+gayatri78910@users.noreply.github.com>
2022-06-30 15:39:59 +0530

    second commit

--- case.sh
+++ case.sh
@@ -1,13 +1,4 @@
-echo please select a floor number
-read floor
-
-case "$floor" in
-       "1") echo first floor
-           ;;
-       "2") echo second floor
-           ;;
-       "3") echo third floor
-           ;;
-esac
+#!bin/bash
+echo this is my first pipeline
 
 
