3115a6952042b8b4b8f9d193d4f732dac55ff399
lalith <abc@gmail.com>
2022-06-30 09:29:10 +0000

    case commit

---
+++ case.sh
@@ -0,0 +1,13 @@
+echo please select a floor number
+read floor
+
+case "$floor" in
+       "1") echo first floor
+           ;;
+       "2") echo second floor
+           ;;
+       "3") echo third floor
+           ;;
+esac
+
+
